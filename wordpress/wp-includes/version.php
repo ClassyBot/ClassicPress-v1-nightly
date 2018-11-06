8436cdc45c7c065232958cec71e838ad52c8b190
ClassyBot <bots@classicpress.net>
2018-11-06 00:01:43 +0000

    Nightly migration build 1.0.0-alpha1+migration.20181105

--- wordpress/wp-includes/version.php
+++ wordpress/wp-includes/version.php
@@ -27,7 +27,7 @@
  *
  * @global string $cp_version
  */
-$cp_version = '1.0.0-alpha1+migration.20181104';
+$cp_version = '1.0.0-alpha1+migration.20181105';
 
 /**
  * Return the ClassicPress version string.
