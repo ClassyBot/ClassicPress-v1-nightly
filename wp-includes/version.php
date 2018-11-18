e24ac8cb06518ba8531598cf2f554307c2131581
ClassyBot <bots@classicpress.net>
2018-11-18 00:02:14 +0000

    Nightly build 1.0.0-alpha1+nightly.20181117

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -27,7 +27,7 @@
  *
  * @global string $cp_version
  */
-$cp_version = '1.0.0-alpha1+nightly.20181116';
+$cp_version = '1.0.0-alpha1+nightly.20181117';
 
 /**
  * Return the ClassicPress version string.
