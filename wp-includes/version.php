fb29832bbc70f0299e1181904cd076242579193e
ClassyBot <bots@classicpress.net>
2019-02-08 00:02:29 +0000

    Nightly build 1.0.0-beta2+nightly.20190207

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -27,7 +27,7 @@
  *
  * @global string $cp_version
  */
-$cp_version = '1.0.0-beta2+nightly.20190206';
+$cp_version = '1.0.0-beta2+nightly.20190207';
 
 /**
  * Return the ClassicPress version string.
