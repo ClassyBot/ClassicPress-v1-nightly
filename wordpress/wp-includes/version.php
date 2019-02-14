92d63bb32c277ba38fe381c211739964e2df9124
ClassyBot <bots@classicpress.net>
2019-02-14 00:01:54 +0000

    Nightly migration build 1.0.0-beta2+migration.20190213

--- wordpress/wp-includes/version.php
+++ wordpress/wp-includes/version.php
@@ -27,7 +27,7 @@
  *
  * @global string $cp_version
  */
-$cp_version = '1.0.0-beta2+migration.20190212';
+$cp_version = '1.0.0-beta2+migration.20190213';
 
 /**
  * Return the ClassicPress version string.
