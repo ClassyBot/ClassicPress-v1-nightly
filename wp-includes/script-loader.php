10835a1fc4b6dd77151a18a489b047e2a2b98c29
ClassyBot <bots@classicpress.net>
2019-02-16 00:02:31 +0000

    Nightly build 1.0.0-beta2+nightly.20190215

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -53,7 +53,7 @@ function classicpress_asset_version( $type = 'script', $handle = null ) {
 	static $default_version;
 
 	if ( empty( $default_version ) ) {
-		$default_version = 'cp_28b0fe99';
+		$default_version = 'cp_a85a5a76';
 	}
 
 	/**
