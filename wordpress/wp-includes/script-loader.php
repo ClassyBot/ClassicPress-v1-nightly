9affa0b963d10ad8e4c9635b1b2dca8ba485c5e7
ClassyBot <bots@classicpress.net>
2024-03-10 00:01:47 +0000

    Nightly migration build 1.7.3+migration.20240309

--- wordpress/wp-includes/script-loader.php
+++ wordpress/wp-includes/script-loader.php
@@ -53,7 +53,7 @@ function classicpress_asset_version( $type = 'script', $handle = null ) {
 	static $default_version;
 
 	if ( empty( $default_version ) ) {
-		$default_version = 'cp_9ae81ab3';
+		$default_version = 'cp_fa85eec0';
 	}
 
 	/**
--- wordpress/wp-includes/version.php
+++ wordpress/wp-includes/version.php
@@ -27,7 +27,7 @@
  *
  * @global string $cp_version
  */
-$cp_version = '1.7.2+migration.20240308';
+$cp_version = '1.7.3+migration.20240309';
 
 /**
  * The WordPress version string
